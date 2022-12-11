38f8b5db1d98b84e45f090a57e9cae4d661e54f7
RustamAhmadov <ahmadov.rustam114@gmail.com>
2022-12-11 10:39:40 +0400

    Initial Commit

---
+++ src/main/java/web_acc/WebAccApplication.java
@@ -0,0 +1,13 @@
+package web_acc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web_acc/WebAccApplicationTests.java
@@ -0,0 +1,13 @@
+package web_acc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
